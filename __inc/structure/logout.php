dedc699126974a9b29a6a149f9c90e1cba8d1995
moreirapenna2 <a17022@bcc.unifal-mg.edu.br>
2020-03-14 18:57:00 -0300

    Logout button fix

--- __inc/structure/logout.php
+++ __inc/structure/logout.php
@@ -3,7 +3,7 @@ include_once("C:\\xampp\\htdocs\\__inc\\config.php");
    session_start();
    
    if(session_destroy()) {
-      $sLocation = "location: ".URL;
-      header($sLocation, true);
+      //$sLocation = "location: ".URL;
+      header("location: /", true);
    }
 ?>
--- __pag/welcome.php
+++ __pag/welcome.php
@@ -14,9 +14,15 @@
 
     <title>GeneticDB - Dashboard</title>
 
+    <!-- jquery -->
+    <script src="/../vendor/jquery/jquery.min.js"></script>
+    <script src="/../vendor/bootstrap/js/bootstrap.js"></script>
+    
     <!-- Custom fonts for this template-->
     <link href="/../vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+ 
+   
 
     <!-- Custom styles for this template-->
     <link href="/../css/bootstrap/bootstrap.css" rel="stylesheet">
@@ -50,10 +56,9 @@
                 <li class="nav-item">
                     <a class="nav-link" href="#">Configurations</a>
                 </li>
-            </ul>
-            <ul class="navbar-nav justify-content-end nav-item-end">
-                <li class="nav-item ">
-                    <a class="nav-link" href="#">Logout</a>
+                <hr class="sidebar-divider">
+                <li class="nav-item justify-content-end nav-item-end">
+                    <a class="nav-link" href="/../__inc/structure/logout.php">Sign out</a>
                 </li>
             </ul>
         </div>
